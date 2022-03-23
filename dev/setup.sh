154fdcfc99e55783302ff95b491bb79df2e9837f
Aleksandar Smigic <smiga287@gmail.com>
2022-03-23 23:22:46 +0100

    FIX 🐛: Set proper path for git hooks

--- dev/setup.sh
+++ dev/setup.sh
@@ -2,7 +2,7 @@
 
 install_hooks() {
   git config core.hooksPath \
-    || (git config core.hooksPath "${BASH_SOURCE%/*}/git_hooks")
+    || (git config core.hooksPath "./dev/git_hooks")
 }
 
 echo "Setting up Git hooks"

9a8e05620c1bae222a930f72ca0971eb565ccb0c
ilija-s <ilijastc062@gmail.com>
2022-04-26 01:19:27 +0200

    NEW 📦: Setup skeleton for request service TUI

--- client/cmd/request-creator/main.go
+++ client/cmd/request-creator/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/matf-pp/2022_MATDAQ/internal/request-service/tui"
+)
 
 func main() {
 	fmt.Println("Request creator")
+
+	m := tui.New()
+
+	p := tea.NewProgram(&m, tea.WithAltScreen())
+
+	if err := p.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }

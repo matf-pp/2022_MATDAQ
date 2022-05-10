d7cfac1a25c143b25e843d456eca3a0f99272d4a
Aleksandar Smigic <smiga287@gmail.com>
2022-05-10 23:33:47 +0200

    IMPROVE 👌: Refactor user-service model

--- user-service/internal/controllers.go
+++ user-service/internal/controllers.go
@@ -1,23 +1,22 @@
 package internal
 
 import (
-	"net/http"
-	"fmt"
-	"strconv"
 	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
+	"net/http"
+	"strconv"
 )
 
 var Ctx = context.Background()
 var Rdb *redis.Client
 
-func InitRedis(){
+func InitRedis() {
 	Rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr: "localhost:6379",
 	})
 
 }
-
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -25,7 +24,6 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	money := r.FormValue("money")
 
 	AddUser(username, money)
-
 
 }
 
@@ -35,19 +33,17 @@ func GMHandler(w http.ResponseWriter, r *http.Request) {
 
 	money := GetMoney(username)
 
-
-
-	fmt.Fprintf(w, "%d\n",money)
+	fmt.Fprintf(w, "%d\n", money)
 }
 
 func DMHandler(w http.ResponseWriter, r *http.Request) {
 
 	username := r.FormValue("username")
 	money, err := strconv.Atoi(r.FormValue("money"))
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
-	DecreseMoney(username, money)
+	DecreaseMoney(username, money)
 
 }
--- user-service/internal/models.go
+++ user-service/internal/models.go
@@ -3,35 +3,26 @@ package internal
 import "github.com/go-redis/redis/v8"
 
 func GetMoney(username string) int {
-
-	val, err := Rdb.Get(Ctx, username).Int()
-
+	money, err := Rdb.Get(Ctx, username).Int()
 	if err == redis.Nil {
 		return 0
 	} else if err != nil {
-		//panic(err)
+		panic(err)
 	}
 
-	return val
+	return money
 }
 
 func AddUser(username string, money string) {
-
 	err := Rdb.Set(Ctx, username, money, 0).Err()
 	if err != nil {
-		//panic(err)
+		panic(err)
 	}
-
 }
 
-func DecreseMoney(username string, money int) {
-
-	val, err := Rdb.Get(Ctx, username).Int()
+func DecreaseMoney(username string, moneyAmount int) {
+	err := Rdb.DecrBy(Ctx, username, int64(moneyAmount)).Err()
 	if err != nil {
-		//panic(err)
+		panic(err)
 	}
-
-	val = val - money
-
-	_ = Rdb.SetXX(Ctx, username, val, 0)
 }
